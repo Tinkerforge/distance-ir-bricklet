14525da3d38f0e50b22c3a57b5890fbc94dbc39a
Matthias Bolte <matthias@tinkerforge.com>
2014-02-28 19:13:22 +0100

    javascript: Break long lines and cleanup whitespace

--- software/examples/javascript/ExampleCallback.js
+++ software/examples/javascript/ExampleCallback.js
@@ -2,30 +2,30 @@ var Tinkerforge = require('tinkerforge');
 
 var HOST = 'localhost';
 var PORT = 4223;
-var UID = 'hHE';// Change to your UID
+var UID = 'hHE'; // Change to your UID
 
-var ipcon = new Tinkerforge.IPConnection();// Create IP connection
-var dist = new Tinkerforge.BrickletDistanceIR(UID, ipcon);// Create device object
+var ipcon = new Tinkerforge.IPConnection(); // Create IP connection
+var dist = new Tinkerforge.BrickletDistanceIR(UID, ipcon); // Create device object
 
 ipcon.connect(HOST, PORT,
     function(error) {
-        console.log('Error: '+error);        
+        console.log('Error: '+error);
     }
-);// Connect to brickd
-
+); // Connect to brickd
 // Don't use device before ipcon is connected
+
 ipcon.on(Tinkerforge.IPConnection.CALLBACK_CONNECTED,
     function(connectReason) {
         // Set Period for distance callback to 0.2s (200ms)
-        // Note: The callback is only called every 200ms if the 
+        // Note: The callback is only called every 200ms if the
         // distance has changed since the last call!
-        dist.setDistanceCallbackPeriod(200);      
+        dist.setDistanceCallbackPeriod(200);
     }
 );
 
-//Register distance callback
+// Register distance callback
 dist.on(Tinkerforge.BrickletDistanceIR.CALLBACK_DISTANCE,
-    //Callback function for distance callback (parameter has unit mm)
+    // Callback function for distance callback (parameter has unit mm)
     function(distance) {
         console.log('Distance: '+distance/10+' cm');
     }
@@ -38,4 +38,3 @@ process.stdin.on('data',
         process.exit(0);
     }
 );
-
--- software/examples/javascript/ExampleThreshold.js
+++ software/examples/javascript/ExampleThreshold.js
@@ -2,30 +2,30 @@ var Tinkerforge = require('tinkerforge');
 
 var HOST = 'localhost';
 var PORT = 4223;
-var UID = 'hHE';// Change to your UID
+var UID = 'hHE'; // Change to your UID
 
-var ipcon = new Tinkerforge.IPConnection();// Create IP connection
-var dist = new Tinkerforge.BrickletDistanceIR(UID, ipcon);// Create device object
+var ipcon = new Tinkerforge.IPConnection(); // Create IP connection
+var dist = new Tinkerforge.BrickletDistanceIR(UID, ipcon); // Create device object
 
 ipcon.connect(HOST, PORT,
     function(error) {
-        console.log('Error: '+error);        
+        console.log('Error: '+error);
     }
-);// Connect to brickd
-
+); // Connect to brickd
 // Don't use device before ipcon is connected
+
 ipcon.on(Tinkerforge.IPConnection.CALLBACK_CONNECTED,
     function(connectReason) {
         // Get threshold callbacks with a debounce time of 10 seconds (10000ms)
         dist.setDebouncePeriod(10000);
         // Configure threshold for "smaller than 20cm" (unit is mm)
-        dist.setDistanceCallbackThreshold('<', 200, 0);       
+        dist.setDistanceCallbackThreshold('<', 200, 0);
     }
 );
 
-//Register threshold reached callback
+// Register threshold reached callback
 dist.on(Tinkerforge.BrickletDistanceIR.CALLBACK_DISTANCE_REACHED,
-    //Callback for distance smaller than 20cm
+    // Callback for distance smaller than 20cm
     function(distance) {
         console.log('Distance is smaller than 20cm: '+distance/10+' cm');
     }
@@ -38,4 +38,3 @@ process.stdin.on('data',
         process.exit(0);
     }
 );
-
