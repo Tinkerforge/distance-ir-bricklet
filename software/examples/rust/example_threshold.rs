774b3b860f01a7a6398363f096b7f62cff948367
Erik Fleckstein <erik@tinkerforge.com>
2018-11-06 15:51:08 +0100

    Add rust examples

---
+++ software/examples/rust/example_threshold.rs
@@ -0,0 +1,36 @@
+use std::{error::Error, io, thread};
+use tinkerforge::{distance_ir_bricklet::*, ipconnection::IpConnection};
+
+const HOST: &str = "127.0.0.1";
+const PORT: u16 = 4223;
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Distance IR Bricklet
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let ipcon = IpConnection::new(); // Create IP connection
+    let distance_ir_bricklet = DistanceIRBricklet::new(UID, &ipcon); // Create device object
+
+    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
+                                        // Don't use device before ipcon is connected
+
+    // Get threshold listeners with a debounce time of 10 seconds (10000ms)
+    distance_ir_bricklet.set_debounce_period(10000);
+
+    //Create listener for distance reached events.
+    let distance_reached_listener = distance_ir_bricklet.get_distance_reached_receiver();
+    // Spawn thread to handle received events. This thread ends when the distance_ir_bricklet
+    // is dropped, so there is no need for manual cleanup.
+    thread::spawn(move || {
+        for event in distance_reached_listener {
+            println!("Distance: {}{}", event as f32 / 10.0, " cm");
+        }
+    });
+
+    // Configure threshold for distance "smaller than 30 cm"
+    distance_ir_bricklet.set_distance_callback_threshold('<', 30 * 10, 0);
+
+    println!("Press enter to exit.");
+    let mut _input = String::new();
+    io::stdin().read_line(&mut _input)?;
+    ipcon.disconnect();
+    Ok(())
+}
